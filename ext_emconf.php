73f24c3d052d2fba03d441a9f530c7909428b495
Fabien Udriot <fabien.udriot@ecodev.ch>
2016-07-24 23:07:15 +0200

    Release 0.9.0 - First public release. See README of https://github.com/fabarea/media_upload for more information

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,17 +2,16 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Media Upload',
-    'description' => 'Fluid widget for (mass) uploading files on the Frontend using HTML5 techniques powered by Fine Uploader - http://fineuploader.com/',
+    'description' => 'Fluid widget for mass uploading files on the Frontend using HTML5 techniques powered by Fine Uploader - http://fineuploader.com/',
     'category' => 'fe',
     'author' => 'Fabien Udriot',
     'author_email' => 'fabien@ecodev.ch',
     'state' => 'beta',
-    'version' => '0.1.0',
+    'version' => '0.9.0',
     'constraints' =>
         [
             'depends' =>
                 [
-                    'media' => '',
                     'typo3' => '7.6.0-7.6.99',
                 ],
             'conflicts' =>
@@ -20,6 +19,7 @@ $EM_CONF[$_EXTKEY] = [
                 ],
             'suggests' =>
                 [
+                    'media' => '',
                     'metadata' => '',
                 ],
         ],
