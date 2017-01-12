1b9473cabddbbd706781514692e52acf497be5fc
Fabien Udriot <fabien@ecodev.ch>
2017-01-12 12:56:05 +0000

    Release 0.9.4 - https://github.com/fabarea/media_upload/compare/0.9.3...0.9.4
    [BUGFIX] Mute exif data related warnings

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Fabien Udriot',
     'author_email' => 'fabien@ecodev.ch',
     'state' => 'beta',
-    'version' => '1.0.0-dev',
+    'version' => '0.9.4',
     'autoload' => [
         'psr-4' => ['Fab\\MediaUpload\\' => 'Classes']
     ],
