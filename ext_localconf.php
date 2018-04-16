2b686d8c7807df4a56d1d8f7112e4f510bc220af
Stuart McFarlane <stuart@mcfarlane.nl>
2018-04-16 13:19:48 +0200

    Retired ext_typoscript_[setup|constant].txt
    The typoscript settings can not be overwritten if you use
    ext_typoscript_[setup|constant].txt because they are loaded last.
    
    Resolves #34

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,20 @@
 <?php
 if (!defined('TYPO3_MODE')) die ('Access denied.');
 
+$configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['media_upload']);
+if (false === isset($configuration['autoload_typoscript']) || true === (bool)$configuration['autoload_typoscript']) {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+        'media_upload',
+        'constants',
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:media_upload/Configuration/TypoScript/constants.typoscript">'
+    );
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+        'media_upload',
+        'setup',
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:media_upload/Configuration/TypoScript/setup.typoscript">'
+    );
+}
+
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
     'Fab.media_upload',
     'Pi1',
---
+++ ext_tables.php
@@ -0,0 +1,8 @@
+<?php
+if (!defined('TYPO3_MODE')) die ('Access denied.');
+
+$configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['media_upload']);
+// Possible Static TS loading
+if (true === isset($configuration['autoload_typoscript']) && true === (bool)$configuration['autoload_typoscript']) {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('media_upload', 'Configuration/TypoScript', 'Media upload');
+}
