268ea6e7fcc749e09aa01297bd9c362ef3441475
Stuart McFarlane <stuart@mcfarlane.nl>
2018-04-16 13:19:48 +0200

    Retired ext_typoscript_[setup|constant].txt
    The typoscript settings can not be overwritten if you use ext_typoscript_[setup|constant].txt
    because they are loaded last.
    
    Please refer to https://usetypo3.com/good-practices-in-extensions.html

---
+++ ext_tables.php
@@ -0,0 +1,4 @@
+<?php
+if (!defined('TYPO3_MODE')) die ('Access denied.');
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Media upload');
