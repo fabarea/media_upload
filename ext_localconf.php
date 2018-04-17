ab711bfe6f29ebcc55ea7b6b616288ff09e537e2
Fabien Udriot <fabien@omic.ch>
2018-04-17 09:26:10 +0200

    Wrap code from ext_localconf and ext_tables within call_user_func

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,31 +1,33 @@
 <?php
 if (!defined('TYPO3_MODE')) die ('Access denied.');
 
-$configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['media_upload']);
-if (false === isset($configuration['autoload_typoscript']) || true === (bool)$configuration['autoload_typoscript']) {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
-        'media_upload',
-        'constants',
-        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:media_upload/Configuration/TypoScript/constants.typoscript">'
-    );
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
-        'media_upload',
-        'setup',
-        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:media_upload/Configuration/TypoScript/setup.typoscript">'
-    );
-}
+call_user_func(function () {
+    $configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['media_upload']);
+    if (false === isset($configuration['autoload_typoscript']) || true === (bool)$configuration['autoload_typoscript']) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+            'media_upload',
+            'constants',
+            '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:media_upload/Configuration/TypoScript/constants.typoscript">'
+        );
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+            'media_upload',
+            'setup',
+            '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:media_upload/Configuration/TypoScript/setup.typoscript">'
+        );
+    }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Fab.media_upload',
-    'Pi1',
-    array(
-        'MediaUpload' => 'upload',
-    ),
-    // non-cacheable actions
-    array(
-        'MediaUpload' => 'upload',
-    )
-);
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'Fab.media_upload',
+        'Pi1',
+        array(
+            'MediaUpload' => 'upload',
+        ),
+        // non-cacheable actions
+        array(
+            'MediaUpload' => 'upload',
+        )
+    );
 
 // Setting up a script that can be run from the cli_dispatch.phpsh script.
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = \Fab\MediaUpload\Command\TemporaryFileCommandController::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = \Fab\MediaUpload\Command\TemporaryFileCommandController::class;
+});
