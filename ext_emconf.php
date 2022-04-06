839fc0e3139673682defd822507479dce4cf0d4b
Fabien Udriot <fabarea@users.noreply.github.com>
2022-04-06 15:08:06 +0200

    [TASK] Increase TYPO3 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
         [
             'depends' =>
                 [
-                    'typo3' => '10.4.0-10.4.99',
+                    'typo3' => '11.5.0-11.5.99',
                 ],
             'conflicts' =>
                 [
