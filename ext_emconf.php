ae425f11ea408fc4f9fc0bb2509a2cd81ff13b7f
Johannes Przymusinski <j.przymusinski@die-lobby.de>
2022-12-14 09:33:53 +0100

    [CHORE] Add version to ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,6 +7,7 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Fabien Udriot',
     'author_email' => 'fabien@ecodev.ch',
     'state' => 'stable',
+    'version' => '3.0.4',
     'autoload' => [
         'psr-4' => ['Fab\\MediaUpload\\' => 'Classes']
     ],
